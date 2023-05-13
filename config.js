35e398544ede0002c55846e44380ca14a5a162be
Divyanshu rawat <divrawat2001@gmail.com>
2023-05-13 13:31:15 +0530

    added config

---
+++ config.js
@@ -0,0 +1,27 @@
+
+const publicRuntimeConfig = {
+    APP_NAME: 'Coding4u',
+    API_DEVELOPMENT: 'http://localhost:8000/api',
+    API_PRODUCTION:"https://coding4u.onrender.com/",
+    API_PRODUCTION:"https://coding4u.onrender.com/api",
+    PRODUCTION: true,
+    DOMAIN_DEVELOPMENT:"http://localhost:3000",
+    DOMAIN_PRODUCTION:"https://coding4u-project.vercel.app/",
+    DISQUS_SHORTNAME:"my-cms-7",
+    }
+
+
+export const API = publicRuntimeConfig.PRODUCTION
+    ? publicRuntimeConfig.API_PRODUCTION
+    : publicRuntimeConfig.API_DEVELOPMENT;
+
+export const APP_NAME = publicRuntimeConfig.APP_NAME;
+
+export const DOMAIN = publicRuntimeConfig.PRODUCTION
+    ? publicRuntimeConfig.DOMAIN_PRODUCTION
+    : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
+
+export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
+export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;
+
+
