a908a07453495e2b4ff691c15b23f52d3793fea7
Divyanshu rawat <divrawat2001@gmail.com>
2023-05-15 13:15:58 +0530

    sferwgf000000000000

--- components/crud/BlogUpdate.js
+++ components/crud/BlogUpdate.js
@@ -317,7 +317,7 @@ const BlogUpdate = ({ router}) => {
                     <div className={styles0.column}>
 
                         <SunEditor
-                            
+                            value={body} 
                             placeholder="Start typing paragraph here .............."
                             onChange={handleBody} height="1500px" setDefaultStyle="font-family:trebuchet ms; color:black;font-size:17px;padding:15px"
                             setOptions={{
