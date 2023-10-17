79c815b707800dea8c155ec757069a063b71b9cc
Divyanshu rawat <divrawat2001@gmail.com>
2023-10-17 12:52:54 +0530

    esfsegfv

---
+++ pages/admin/tag.js
@@ -0,0 +1,13 @@
+import Admin from '../../components/auth/Admin';
+import Tag from '../../components/crud/Tag';
+
+const tag= () => {
+  return (
+     <Admin>
+      <Tag/>
+      </Admin>
+      
+  )
+}
+
+export default tag
