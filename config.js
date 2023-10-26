97d360c846732eca0f4dfae51dd7435fb524ca0a
Divyanshu rawat <divrawat2001@gmail.com>
2023-10-26 22:42:18 +0530

    sef5segf

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-// export const API = "https://backend-coding4u.vercel.app/api";
-// export const DOMAIN = "https://coding4u-project.vercel.app";
+ export const API = "https://backend-coding4u.vercel.app/api";
+ export const DOMAIN = "https://coding4u-project.vercel.app";
 export const MY_API="https://backend-coding4u.vercel.app";
 
 export const APP_NAME = "Coding4u";
 export const DISQUS_SHORTNAME = "my-cms-7";
 export const APP_DESCRIPTION = "Coding4U is a blog website where users can publish their articles related to programming or computer science field. It is designed to help individuals learn programming concepts, languages, and best practices."
 
-      export const API = 'http://localhost:8000/api'
-     export const DOMAIN = "http://localhost:3000";
+      // export const API = 'http://localhost:8000/api'
+//      export const DOMAIN = "http://localhost:3000";
