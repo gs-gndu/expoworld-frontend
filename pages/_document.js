aa8ef98eaf592c2978871dd7dde75872054df29a
Divyanshu rawat <divrawat2001@gmail.com>
2023-10-28 21:46:31 +0530

    sdfv0sdgv0sd0gv000

--- pages/_document.js
+++ pages/_document.js
@@ -101,9 +101,12 @@ export default class MyDocument extends Document {
           }} />
         </Head>
         <body>
-
-          {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54XZPTFN"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
+<script dangerouslySetInnerHTML={{
+            __html: `
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54XZPTFN"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+`
+          }} />
 
           <Main />
           <NextScript />
