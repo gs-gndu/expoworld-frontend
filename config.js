4146555d3e887b58c14619bc148ece016e866cd4
gs-gndu <2020010004100@gndu.ac.in>
2025-05-03 16:52:08 +0530

    sd0vs5

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
-// export const API = "https://backend-coding4u.vercel.app/api";
-//export const DOMAIN = "https://coding4u-project.vercel.app";
-export const BACKEND = "https://backend-coding4u.vercel.app";
+export const API = "https://expoworld-backend.vercel.app/api";
+export const BACKEND = "https://expoworld-backend.vercel.app";
+export const DOMAIN = "https://expoworld-frontend.vercel.app";
 
 
-export const API = 'http://localhost:8000/api';
-export const DOMAIN = "http://localhost:3000";
+
+// export const API = 'http://localhost:8000/api';
+// export const DOMAIN = "http://localhost:3000";
 // export const BACKEND = "http://localhost:8000";
 
 
@@ -12,4 +13,4 @@ export const DOMAIN = "http://localhost:3000";
 export const APP_NAME = "EXPOWORLD.COM";
 export const DISQUS_SHORTNAME = "my-cms-7";
 export const APP_DESCRIPTION = "Expo World WEBSITE IS THE  PLATFORM WHERE USER CAN SHARE THE RESOURSE."
-export const MY_API = "https://backend-coding4u.vercel.app";
+export const MY_API = "https://expoworld-backend.vercel.app";
