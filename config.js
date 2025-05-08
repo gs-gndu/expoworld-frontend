1cb9a7712727b20bbeb830ecb6f5cd61f766cc5e
gs-gndu <2020010004100@gndu.ac.in>
2025-05-08 14:13:00 +0530

    u

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ export const DOMAIN = "https://expoworld-frontend.vercel.app";
 
 
 
-// export const API = 'http://localhost:8000/api';
+//export const API = 'http://localhost:8000/api';
 // export const DOMAIN = "http://localhost:3000";
-// export const BACKEND = "http://localhost:8000";
+//export const BACKEND = "http://localhost:8000";
 
 
 
